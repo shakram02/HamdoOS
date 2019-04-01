3f38169fe7c849095127b053ee6e32ebada51783
shakram02 <ahmedhamdyau@gmail.com>
2019-04-01 21:54:03 +0200

    output a .o bootloader

--- bootloader/compile_bootloader.sh
+++ bootloader/compile_bootloader.sh
@@ -1,7 +1,7 @@
 set -e # Exit on error
 
 echo "Compiling assembly file..."
-nasm -f bin bootloader.asm -o bootloader
+nasm -f bin bootloader.asm -o bootloader.o
 
 echo "Making disk image..."
 dd if=/dev/zero of=disk.img bs=512 count=2880
@@ -11,4 +11,4 @@ echo "Writing bootloader to the 1st sector of the drive..."
 # The option conv=notrunc preserves the original size of the floppy disk.
 # Without this option, the 1.4 MB disk image will be completely replaced
 # by the new disk.img with only 512 bytes
-dd conv=notrunc if=bootloader of=disk.img bs=512 count=1 seek=0
+dd conv=notrunc if=bootloader.o of=disk.img bs=512 count=1 seek=0
