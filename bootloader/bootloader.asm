d895140fe2952000a17de07c68a637b7dc1d2c1b
shakram02 <ahmedhamdyau@gmail.com>
2019-04-01 22:22:37 +0200

    refactor file to use only needed functions

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -7,63 +7,18 @@ bits 16    ; 16-bit mode
 
 jmp init
 
-; %include "io.asm"
-
-; start:
-; jmp boot
-
-
-; printMsg:
-; mov BP, msg
-; call Print
-; ret
+%include "io.asm"
 
 init:
     cli    ; Disable interrupts
     mov si, msg
-    call mov_cursor
+    call move_cursor
     call print_char
-
-    mov dl, 0x00
-    mov al, 0x40
-
-    .print_alphabet:
-    add al, 0x01
-    mov ah, 0x0E
-    int 0x10
-    cmp al, 'Z'
-    jne .print_alphabet
-
     hlt
 
-print_char:
-    lodsb
-    cmp al, 0x0
-    je .done
-    mov ah, 0x0E
-    int 0x10
-    jmp print_char
-    .done ret
+msg: db "Welcome to Ovizivo Operating System!", 0x0, 0x0
 
-mov_cursor:
-    ; Move the cursor to a specified position to write
-    ; mov BH, 0x00
-    mov DH, 0x0F
-    mov DL, 0x0A
-    mov ah, 0x02
-    int 0x10
-    ret
-
-    ; boot:
-    ; cli
-    ; cld ; clear direction flag
-
-    ; hlt ; halt the system
-
-    ; The bootloader has to be 512 byets. Clear the rest of
-    ; the bytes with 0
-
-msg: db "Welcome to Ovizo Operating System!", 0x0, 0x0
-
+; The bootloader has to be 512 byets. Clear the rest of
+; the bytes with 0
 times 510 - ($-$$) db 0
 dw 0xAA55  ; Boot signature
