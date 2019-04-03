2de995381aed202c9723c3f3a355c2f26422ccdf
shakram02 <ahmedhamdyau@gmail.com>
2019-04-03 22:25:56 +0200

    comment unused functions

--- bootloader/io.asm
+++ bootloader/io.asm
@@ -4,11 +4,12 @@
 ; *********************
 bits 16          ; 16-bit mode
 
-get_cursor:
-    ; Load information about the cursor, I'll use that with gdb to get info
-    mov BH, 0
-    mov AH, 0x03
-    int 0x10
+; get_cursor:
+;     ; Load information about the cursor, I'll use that with gdb to get info
+;     mov BH, 0
+;     mov AH, 0x03
+;     int 0x10
+; 	ret
 
 move_cursor:
     ; Move the cursor to a specified position to write
@@ -18,20 +19,21 @@ move_cursor:
     int 0x10
     ret
 
-print_char:
+print_chars:
     ; Prints a string to the screen, character by character
     lodsb
     cmp al, 0x0
     je .done
     mov ah, 0x0E
     int 0x10
-    jmp print_char
+    jmp print_chars
     .done ret
 
-print_string:
-    ; Prints a string to the screen
-    mov AL, 0x01 ; Update cursor after writing
-    mov DH, 0x10
-    mov DL, 0x00
-    mov AH, 0x13
-    int 0x10
+; print_string:
+;     ; Prints a string from bp to the screen
+;     mov AL, 0x01 ; Update cursor after writing
+;     mov DH, 0x0F
+;     mov DL, 0x0F
+;     mov AH, 0x13 ; function 13 - write string
+;     int 0x10
+; 	ret
