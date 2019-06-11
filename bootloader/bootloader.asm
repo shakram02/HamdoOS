f7cf84f43a7a31b8f7873e9abff35211601b2dba
shakram02 <ahmedhamdyau@gmail.com>
2019-06-11 20:22:50 +0200

    hack the bootloader into loading the OS correctly by manipulating the address manually

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -2,38 +2,50 @@
 ; bootloader.asm
 ; Simple bootloader
 ; *********************
-bits 16                 ; 16-bit mode
-; org 0x7C00              ; Starting address after PROC finishes
-
+bits 16                          ; 16-bit mode
+                                 ; org 0x7C00              ; Starting address after PROC finishes (set by linker)
+xor ax, ax                       ; Set the data segment
+mov ds, ax
 jmp init
 
 load_bootloader:
-	mov ah, 0x2         ; Read sectors from drive [BIOS Interrupt]
-    mov al, 0x2         ; Read 2 sectors from drive
-    mov ch, 0x0         ; Cylinder 0
-    mov cl, 0x2         ; Sector 2
-    mov dh, 0x0         ; Head 0
+    mov ah, 0x2                  ; Read sectors from drive [BIOS Interrupt]
+    mov al, 0x5                  ; Read sectors from drive
+    mov ch, 0x0                  ; Cylinder 0
+    mov cl, 0x2                  ; Sector 2
+    mov dh, 0x0                  ; Head 0
 
-    mov bx, bootloader_load_addr
-    mov es, bx      ; ES = 0x500
-    xor bx, bx      ; Clear BX
-    int 0x13        ; call BIOS
-    ret             ; You can only be guaranteed that your bootloader will be loaded and run from physical address 0x00007c00
-                        ; and that the boot drive number is loaded into the DL register.
-                        ; https://stackoverflow.com/questions/34178717/load-segment-from-floppy-with-int13h
+    mov bx, bootloader_load_addr ; The address is calculated as: 16 * ES + BX
+    mov es, bx                   ; ES = 0x500
+    xor bx, bx                   ; Clear BX
+    int 0x13                     ; call BIOS
+    ret                          ; You can only be guaranteed that your bootloader will be loaded and run from physical address 0x00007c00
+                                 ; and that the boot drive number is loaded into the DL register.
+                                 ; https://stackoverflow.com/questions/34178717/load-segment-from-floppy-with-int13h
 
 
 init:
-    cli                 ; Disable interrupts
-    cld                 ; Set the direction flag to be positive direction
-                        ; call move_cursor
+    cli                          ; Disable interrupts
+    cld                          ; Set the direction flag to be positive direction
+                                 ; call move_cursor
+
+                                 ; start the stack right before the bootloader (stack grows downwards)
+                                 ; https://wiki.osdev.org/My_Bootloader_Does_Not_Work
+    mov ax, 0
+    mov ss, ax
+    mov sp, 0x7c00
+
     mov si, msg
     call print_string
 
-	call load_bootloader
-    jc print_fail       ; Jump if CF is set (error) ( return value from interrupt )
-
-    jmp bootloader_load_addr:0x0        ; jump and execute the code
+    call load_bootloader
+    jc print_fail                ; Jump if CF is set (error) ( return value from interrupt )
+    mov ax, bootloader_load_addr
+    mov bx, 0x10
+    mul bx
+    mov bx,0x100
+    add ax,bx
+    jmp ax                       ; jump and execute the code @ 0x7c2f
 
 print_fail:
     mov si, fail_msg
@@ -44,11 +56,11 @@ print_fail:
 
 msg: db "Welcome to HamdoOS!", 0x0D, 0x0A, 0x0
 fail_msg: db "Failed to read disk", 0x0D, 0x10, 0x0
-bootloader_load_addr: equ 0x0500
+bootloader_load_addr: equ 0x00000500
 
 %include "../asm_utils/io.asm"
 
 ; The bootloader has to be 512 byets. Clear the rest of
 ; the bytes with 0
 times 510 - ($-$$) db 0x0
-dw 0xAA55               ; Boot signature
+dw 0xAA55                        ; Boot signature
