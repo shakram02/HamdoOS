3c41bda4d868b272b4519ad2c543f66b789c0184
shakram02 <ahmedhamdyau@gmail.com>
2019-06-26 07:35:58 +0200

    restore flat binary format

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -3,12 +3,15 @@
 ; Simple bootloader
 ; *********************
 bits 16                          ; 16-bit mode
-                                 ; org 0x7C00              ; Starting address after PROC finishes (set by linker)
-xor ax, ax                       ; Set the data segment
-mov ds, ax
+org 0x7C00              ; Starting address (set by the linker script)
+
+mov ax, 0
+mov ss, ax
+mov sp, $
+
 jmp init
 
-load_bootloader:
+load_os:
     mov ah, 0x2                  ; Read sectors from drive [BIOS Interrupt]
     mov al, 0x5                  ; Read sectors from drive
     mov ch, 0x0                  ; Cylinder 0
@@ -16,8 +19,8 @@ load_bootloader:
     mov dh, 0x0                  ; Head 0
 
     mov bx, bootloader_load_addr ; The address is calculated as: 16 * ES + BX
-    mov es, bx                   ; ES = 0x500
-    xor bx, bx                   ; Clear BX
+    mov es, bx
+    xor bx, bx
     int 0x13                     ; call BIOS
     ret                          ; You can only be guaranteed that your bootloader will be loaded and run from physical address 0x00007c00
                                  ; and that the boot drive number is loaded into the DL register.
@@ -31,28 +34,31 @@ init:
 
                                  ; start the stack right before the bootloader (stack grows downwards)
                                  ; https://wiki.osdev.org/My_Bootloader_Does_Not_Work
-    mov ax, 0
-    mov ss, ax
-    mov sp, 0x7c00
+
 
     mov si, msg
     call print_string
 
-    call load_bootloader
+    ; http://3zanders.co.uk/2017/10/16/writing-a-bootloader2/
+    ; mov ax, 0x2401
+    ; int 0x15 ; enable A20 bit
+    
+    ; mov ax, 0x3
+    ; int 0x10 ; set vga text mode 3
+
+    call load_os
     jc print_fail                ; Jump if CF is set (error) ( return value from interrupt )
-    jmp 0x5100                   ; Can't adjust the address using ES:NX for some reason, will leave it hardcoded :3
-                                 ; TODO: gdb isn't showing any sources
+
+    jmp 0x500
 
 print_fail:
     mov si, fail_msg
     jmp print_string
     hlt
 
-    ; TODO: print a single char
-
 msg: db "Welcome to HamdoOS!", 0x0D, 0x0A, 0x0
-fail_msg: db "Failed to read disk", 0x0D, 0x10, 0x0
-bootloader_load_addr: equ 0x500
+fail_msg: db 0x0,"Failed to read disk", 0x0D, 0x10, 0x0
+bootloader_load_addr: equ 0x050
 
 %include "../asm_utils/io.asm"
 
