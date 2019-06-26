9b443d216d3a780afd99455ab31de806c0af2df9
shakram02 <ahmedhamdyau@gmail.com>
2019-06-26 08:30:39 +0200

    saner print with comments
    save and restore register states

--- asm_utils/io.asm
+++ asm_utils/io.asm
@@ -2,22 +2,35 @@
 ; io.asm
 ; I/O related routines
 ; *********************
-bits 16          ; 16-bit mode
+bits 16 ; 16-bit mode
 
 move_cursor:
+    push ax
+    push dx
     ; Move the cursor to a specified position to write
-    mov DH, 0x0F
-    mov DL, 0x0A
+    mov dh, 0x0F
+    mov dl, 0x0A
     mov ah, 0x02
     int 0x10
+    pop dx
+    pop ax
     ret
 
 print_string:
-    ; Prints a string to the screen, character by character
-    lodsb
-    cmp al, 0x0
-    je .done
+    push ax
     mov ah, 0x0E
+    
+.print_loop:
+    ; Loads a byte (starting from SI) into AL, 
+    ; pointer is auto-incremented according to direction flag (DF)
+    lodsb   
+    ; Prints a string to the screen, character by character
+    cmp al, 0x0
+    je print_string.done
+
     int 0x10
-    jmp print_string
-    .done ret
+    jmp print_string.print_loop
+
+.done:
+    pop ax
+    ret
